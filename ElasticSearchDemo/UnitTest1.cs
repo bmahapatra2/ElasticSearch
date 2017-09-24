e9eb98212207dbadd899f178bf3bb2d6e555675b
Bandana Mahapatra <bmahapatra@tavisca.com>
2017-09-24 23:51:30 +0530

    Elastic Search

---
+++ ElasticSearchDemo/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Nest;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Flight;
+using System.Linq;
+
+namespace ElasticSearchDemo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public UnitTest1()
+        {   
+            ElasticSearchImplementation search = new ElasticSearchImplementation();
+            search.AddNewIndex(new FlightDetails(1, "airindia",  "india"));
+            search.AddNewIndex(new FlightDetails(2, "jetairways",  "delhi"));
+            search.AddNewIndex(new FlightDetails(3, "indigo", "mumbai"));
+            search.AddNewIndex(new FlightDetails(4, "emirates",  "chennai"));
+            search.AddNewIndex(new FlightDetails(5, "lufthansa", "kolkata"));
+            search.AddNewIndex(new FlightDetails(4, "spicejet",  "indore"));
+        }
+    
+        [TestMethod]
+        public void GetResultTest()
+        {
+            ElasticSearchImplementation search = new ElasticSearchImplementation();
+            FlightDetails details = new FlightDetails();
+            var result = search.GetResult();
+            //result.Contains("airindia";
+            Assert.IsFalse(result.FirstOrDefault<FlightDetails>(x => x.FlightName == "airindia") != null);
+            Assert.IsFalse(result.FirstOrDefault<FlightDetails>(x => x.FlightName == "jetairways") != null);
+            Assert.IsFalse(result.FirstOrDefault<FlightDetails>(x => x.FlightName == "indigo") != null);
+        }
+    }
+}
